c8f62db62463a192a30c365f6422c6688ff8ddf1
DariaLaz <daria.lazarova03@gmail.com>
2024-03-16 21:46:38 +0200

    clock

--- hackaton-game/src/scenes/GameScene.js
+++ hackaton-game/src/scenes/GameScene.js
@@ -1,323 +1,340 @@
 class GameScene extends Phaser.Scene {
-    constructor() {
-        super("GameScene");
-    }
-    
-    preload() {
-        this.load.image("bg", "assets/bg.png");
-        this.load.image("chair", "assets/chair.png")
-        this.load.image('drawers', 'assets/drawers.png')
-this.load.image('remote', 'assets/remote.png')
-        this.load.spritesheet('bulb', 'assets/bulb.png', {
-            frameWidth: 53.6,
-            frameHeight: 100,
-        });
-        this.load.spritesheet('battery', 'assets/battery.png', {
-            frameWidth: 47.1,
-            frameHeight: 100,
-        });
-        this.load.spritesheet('fan', 'assets/fan.png', {
-            frameWidth: 46,
-            frameHeight: 50,
-        });
-        this.load.image('table', 'assets/table.png');
-        this.load.image('waterbowl', 'assets/waterbowl.png');
-        this.load.image('bookshelf', 'assets/bookshelf.png');
-    }
-    create() {
-        
-        this.bg = this.add.tileSprite(0, 0, this.sys.game.config.width, this.sys.game.config.height, "bg").setOrigin(0, 0);
-        this.bg.setDepth(-100);
-        this.bg.setScrollFactor(0);
-        
-        this.physics.world.setBounds(0, -100, 3000, 1000);
+  constructor() {
+    super("GameScene");
+  }
 
-        this.createFloor();
+  preload() {
+    this.load.image("bg", "assets/bg.png");
+    this.load.image("chair", "assets/chair.png");
+    this.load.image("drawers", "assets/drawers.png");
+    this.load.image("remote", "assets/remote.png");
+    this.load.spritesheet("bulb", "assets/bulb.png", {
+      frameWidth: 53.6,
+      frameHeight: 100,
+    });
+    this.load.spritesheet("battery", "assets/battery.png", {
+      frameWidth: 47.1,
+      frameHeight: 100,
+    });
+    this.load.spritesheet("fan", "assets/fan.png", {
+      frameWidth: 46,
+      frameHeight: 50,
+    });
+    this.load.image("table", "assets/table.png");
+    this.load.image("waterbowl", "assets/waterbowl.png");
+    this.load.image("bookshelf", "assets/bookshelf.png");
+    this.load.image("clock", "assets/clock.png");
+  }
+  create() {
+    this.bg = this.add
+      .tileSprite(
+        0,
+        0,
+        this.sys.game.config.width,
+        this.sys.game.config.height,
+        "bg"
+      )
+      .setOrigin(0, 0);
+    this.bg.setDepth(-100);
+    this.bg.setScrollFactor(0);
 
-        this.createBulb();
-        this.createBattery();
+    this.physics.world.setBounds(0, -100, 3000, 1000);
 
-        this.createChair();
-        this.createDrawers();
+    this.createFloor();
 
-        this.createWaterBowl();
-        this.createBookshelf();
-this.createRemote();
+    this.createBulb();
+    this.createBattery();
 
-        this.createFan();
-        this.createTable();
+    this.createChair();
+    this.createDrawers();
 
-        this.keys = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.W,
-            left: Phaser.Input.Keyboard.KeyCodes.A,
-            down: Phaser.Input.Keyboard.KeyCodes.S,
-            right: Phaser.Input.Keyboard.KeyCodes.D,
+    this.createWaterBowl();
+    this.createBookshelf();
+    this.createRemote();
 
-            p2up: Phaser.Input.Keyboard.KeyCodes.UP,
-            p2left: Phaser.Input.Keyboard.KeyCodes.LEFT,
-            p2down: Phaser.Input.Keyboard.KeyCodes.DOWN,
-            p2right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-        });
+    this.createFan();
+    this.createTable();
 
-    }   
+    this.createClock();
 
-    createFloor() {
-        this.floor = this.physics.add.staticSprite(500, 770, 'floor');
-        this.floor.setScale(1000, 1);
-        this.floor.setDepth(-1000);
-        this.floor.refreshBody(); 
-    }
+    this.keys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
 
-    createChair() {
-        this.chair = this.physics.add.staticSprite(700, 560, 'chair');
-        this.chair.setDepth(-1);
-        this.chair.setScale(3.4);
+      p2up: Phaser.Input.Keyboard.KeyCodes.UP,
+      p2left: Phaser.Input.Keyboard.KeyCodes.LEFT,
+      p2down: Phaser.Input.Keyboard.KeyCodes.DOWN,
+      p2right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+    });
+  }
 
-        this.chairCussion = this.physics.add.staticSprite(700, 600);
-        this.chairCussion.setDepth(-1000);
-        this.chairCussion.setScale(12, 3);
-        this.chairCussion.refreshBody();
+  createFloor() {
+    this.floor = this.physics.add.staticSprite(500, 770, "floor");
+    this.floor.setScale(1000, 1);
+    this.floor.setDepth(-1000);
+    this.floor.refreshBody();
+  }
 
-        this.physics.add.collider(this.bulb, this.chairCussion);
-        this.physics.add.collider(this.battery, this.chairCussion)
-    }
+  createChair() {
+    this.chair = this.physics.add.staticSprite(700, 560, "chair");
+    this.chair.setDepth(-1);
+    this.chair.setScale(3.4);
 
-    createDrawers() {
-        this.drawers = this.physics.add.staticSprite(1250, 520, 'drawers');
-        this.drawers.setDepth(-1);
-        this.drawers.setScale(3.5);
+    this.chairCussion = this.physics.add.staticSprite(700, 600);
+    this.chairCussion.setDepth(-1000);
+    this.chairCussion.setScale(12, 3);
+    this.chairCussion.refreshBody();
 
-        this.drawersBody = this.physics.add.staticSprite(1225, 470);
-        this.drawersBody.setDepth(-1000);
-        this.drawersBody.setScale(18.6, 11);
-        this.drawersBody.refreshBody();
+    this.physics.add.collider(this.bulb, this.chairCussion);
+    this.physics.add.collider(this.battery, this.chairCussion);
+  }
 
-        this.physics.add.collider(this.bulb, this.drawersBody);
-        this.physics.add.collider(this.battery, this.drawersBody)
-    }
+  createDrawers() {
+    this.drawers = this.physics.add.staticSprite(1250, 520, "drawers");
+    this.drawers.setDepth(-1);
+    this.drawers.setScale(3.5);
 
-    createRemote() {
-        this.remote = this.physics.add.staticSprite(1000, 210, 'remote');
-        this.remote.setDepth(10);
-        this.remote.setScale(2);
-    }
+    this.drawersBody = this.physics.add.staticSprite(1225, 470);
+    this.drawersBody.setDepth(-1000);
+    this.drawersBody.setScale(18.6, 11);
+    this.drawersBody.refreshBody();
 
-    createFan() {
-        this.fan = this.physics.add.staticSprite(1640, 700, 'fan');
-        this.fan.setDepth(-1);
-        this.fan.setScale(3.5);
+    this.physics.add.collider(this.bulb, this.drawersBody);
+    this.physics.add.collider(this.battery, this.drawersBody);
+  }
 
-        this.fanBody = this.physics.add.staticSprite(1640, 740);
-        this.fanBody.setDepth(-1000);
-        this.fanBody.setScale(4, 1.2);
-        this.fanBody.refreshBody();
+  createRemote() {
+    this.remote = this.physics.add.staticSprite(1000, 210, "remote");
+    this.remote.setDepth(10);
+    this.remote.setScale(2);
+  }
 
-        this.fanStream = this.physics.add.staticSprite(1640, 540);
-        this.fanStream.setDepth(-1000);
-        this.fanStream.setScale(4, 25);
-        this.fanStream.refreshBody();
+  createFan() {
+    this.fan = this.physics.add.staticSprite(1640, 700, "fan");
+    this.fan.setDepth(-1);
+    this.fan.setScale(3.5);
 
-        this.anims.create({
-            key: 'fanOn',
-            frames: this.anims.generateFrameNumbers('fan', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });        
+    this.fanBody = this.physics.add.staticSprite(1640, 740);
+    this.fanBody.setDepth(-1000);
+    this.fanBody.setScale(4, 1.2);
+    this.fanBody.refreshBody();
 
-        this.anims.create({
-            key: 'fanOff',
-            frames: this.anims.generateFrameNumbers('fan', { start: 4, end: 4 }),
-            frameRate: 10,
-            repeat: -1
-        });
+    this.fanStream = this.physics.add.staticSprite(1640, 540);
+    this.fanStream.setDepth(-1000);
+    this.fanStream.setScale(4, 25);
+    this.fanStream.refreshBody();
 
-        this.physics.add.collider(this.bulb, this.fanBody);
-        this.physics.add.collider(this.battery, this.fanBody)
+    this.anims.create({
+      key: "fanOn",
+      frames: this.anims.generateFrameNumbers("fan", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
 
-    }
+    this.anims.create({
+      key: "fanOff",
+      frames: this.anims.generateFrameNumbers("fan", { start: 4, end: 4 }),
+      frameRate: 10,
+      repeat: -1,
+    });
 
-    createTable() {
-        this.table = this.physics.add.staticSprite(2200, 700, 'table');
-        this.table.setDepth(-1);
-        this.table.setScale(5, 5);
+    this.physics.add.collider(this.bulb, this.fanBody);
+    this.physics.add.collider(this.battery, this.fanBody);
+  }
 
-        this.tableBody = this.physics.add.staticSprite(2200, 700);
-        this.tableBody.setDepth(-1000);
-        this.tableBody.setScale(27, 21);
-        this.tableBody.refreshBody();
+  createTable() {
+    this.table = this.physics.add.staticSprite(2200, 700, "table");
+    this.table.setDepth(-1);
+    this.table.setScale(5, 5);
 
-        this.physics.add.collider(this.bulb, this.tableBody);
-        this.physics.add.collider(this.battery, this.tableBody)
-    }
+    this.tableBody = this.physics.add.staticSprite(2200, 700);
+    this.tableBody.setDepth(-1000);
+    this.tableBody.setScale(27, 21);
+    this.tableBody.refreshBody();
 
-    createWaterBowl() {
-        this.waterbowl = this.physics.add.staticSprite(2250, 325, 'waterbowl');
-        this.waterbowl.setDepth(-1);
-        this.waterbowl.setScale(2);
+    this.physics.add.collider(this.bulb, this.tableBody);
+    this.physics.add.collider(this.battery, this.tableBody);
+  }
 
-        this.waterbowlBody = this.physics.add.staticSprite(2245, 325);
-        this.waterbowlBody.setDepth(-1000);
-        this.waterbowlBody.setScale(0.5);
-        this.waterbowlBody.refreshBody();
+  createWaterBowl() {
+    this.waterbowl = this.physics.add.staticSprite(2250, 325, "waterbowl");
+    this.waterbowl.setDepth(-1);
+    this.waterbowl.setScale(2);
 
-        this.physics.add.collider(this.bulb, this.waterbowlBody);
-        this.physics.add.collider(this.battery, this.waterbowlBody)
-    }
+    this.waterbowlBody = this.physics.add.staticSprite(2245, 325);
+    this.waterbowlBody.setDepth(-1000);
+    this.waterbowlBody.setScale(0.5);
+    this.waterbowlBody.refreshBody();
 
-    createBookshelf() {
-        this.bookshelf = this.physics.add.staticSprite(2015, 260, 'bookshelf');
-        this.bookshelf.setDepth(-1);
-        this.bookshelf.setScale(3);
+    this.physics.add.collider(this.bulb, this.waterbowlBody);
+    this.physics.add.collider(this.battery, this.waterbowlBody);
+  }
 
-        this.bookshelfBody = this.physics.add.staticSprite(2015, 230);
-        this.bookshelfBody.setDepth(-1000);
-        this.bookshelfBody.setScale(6, 2);
-        this.bookshelfBody.refreshBody();
+  createBookshelf() {
+    this.bookshelf = this.physics.add.staticSprite(2015, 260, "bookshelf");
+    this.bookshelf.setDepth(-1);
+    this.bookshelf.setScale(3);
 
-        this.physics.add.collider(this.bulb, this.bookshelfBody);
-        this.physics.add.collider(this.battery, this.bookshelfBody)
-    }
+    this.bookshelfBody = this.physics.add.staticSprite(2015, 230);
+    this.bookshelfBody.setDepth(-1000);
+    this.bookshelfBody.setScale(6, 2);
+    this.bookshelfBody.refreshBody();
 
-    createBulb() {
-        this.bulb = this.physics.add.sprite(100, 200, 'bulb');
+    this.physics.add.collider(this.bulb, this.bookshelfBody);
+    this.physics.add.collider(this.battery, this.bookshelfBody);
+  }
 
-        this.anims.create({
-            key: 'bulbWalk',
-            frames: this.anims.generateFrameNumbers('bulb', { start: 0, end: 7 }),
-            frameRate: 10,
-            repeat: -1
-        });
+  createClock() {
+    this.clock = this.physics.add.staticSprite(2350, 125, "clock");
+    this.clock.setDepth(-1);
+    this.clock.setScale(1.5);
 
-        this.physics.add.collider(this.bulb, this.floor);
-        this.bulb.setCollideWorldBounds(true);
-    }
+    this.clockBody = this.physics.add.staticSprite(2350, 125);
+    this.clockBody.setDepth(-1000);
+    this.clockBody.setScale(3.5);
+    this.clockBody.refreshBody();
 
-    
-
-    createBattery() {
-
-        this.battery = this.physics.add.sprite(100, 200, 'battery');
-
-        this.anims.create({
-            key: 'batteryWalk',
-            frames: this.anims.generateFrameNumbers('battery', { start: 0, end: 5 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'batteryJump',
-            frames: this.anims.generateFrameNumbers('battery', { start: 6, end: 6 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.physics.add.collider(this.battery, this.floor);
-        this.battery.setCollideWorldBounds(true);
+    this.physics.add.collider(this.bulb, this.clockBody);
+    this.physics.add.collider(this.battery, this.clockBody);
     }
 
-   
+  createBulb() {
+    this.bulb = this.physics.add.sprite(2000, 200, "bulb");
 
-    update() {
-        this.updateBulb();
-        this.updateBattery();
-        
-        var isRemoteOn = this.physics.overlap(this.battery, this.remote);
-        this.fan.isOn = isRemoteOn;
+    this.anims.create({
+      key: "bulbWalk",
+      frames: this.anims.generateFrameNumbers("bulb", { start: 0, end: 7 }),
+      frameRate: 10,
+      repeat: -1,
+    });
 
-        this.updateFanBulbCollision();
-        this.updateCamera();
+    this.physics.add.collider(this.bulb, this.floor);
+    this.bulb.setCollideWorldBounds(true);
+  }
 
-        //TODO add dying logic from the water bowl
+  createBattery() {
+    this.battery = this.physics.add.sprite(2000, 200, "battery");
+
+    this.anims.create({
+      key: "batteryWalk",
+      frames: this.anims.generateFrameNumbers("battery", { start: 0, end: 5 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "batteryJump",
+      frames: this.anims.generateFrameNumbers("battery", { start: 6, end: 6 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.physics.add.collider(this.battery, this.floor);
+    this.battery.setCollideWorldBounds(true);
+  }
+
+  update() {
+    this.updateBulb();
+    this.updateBattery();
+
+    var isRemoteOn = this.physics.overlap(this.battery, this.remote);
+    this.fan.isOn = isRemoteOn;
+
+    this.updateFanBulbCollision();
+    this.updateCamera();
+
+    //TODO add dying logic from the water bowl
+  }
+
+  updateBulb() {
+    const speed = 160;
+    this.bulb.play("bulbWalk", true);
+
+    if (this.keys.left.isDown) {
+      this.bulb.setVelocityX(-speed);
+      if (!this.bulb.body.touching.down) {
+        this.bulb.anims.stop();
+      } else {
+        this.bulb.play("bulbWalk", true);
+      }
+    } else if (this.keys.right.isDown) {
+      this.bulb.setVelocityX(speed);
+      if (!this.bulb.body.touching.down) {
+        this.bulb.anims.stop();
+      } else {
+        this.bulb.play("bulbWalk", true);
+      }
+    } else {
+      this.bulb.setVelocityX(0);
+      if (!this.bulb.body.touching.down) {
+        this.bulb.anims.stop();
+      } else {
+        this.bulb.play("bulbWalk", true);
+      }
     }
 
-    updateBulb() {
-        const speed = 160;
-        this.bulb.play('bulbWalk', true);
-  
-        if (this.keys.left.isDown) {
-            this.bulb.setVelocityX(-speed);
-            if (!this.bulb.body.touching.down){
-                this.bulb.anims.stop();
-            } else {
-                this.bulb.play('bulbWalk', true);
-            }
-        } else if (this.keys.right.isDown) {
-            this.bulb.setVelocityX(speed);
-            if (!this.bulb.body.touching.down){
-                this.bulb.anims.stop();
-            } else {
-                this.bulb.play('bulbWalk', true);
-            }
-        } else {
-            this.bulb.setVelocityX(0);
-            if (!this.bulb.body.touching.down){
-                this.bulb.anims.stop();
-            }  else {
-                this.bulb.play('bulbWalk', true);
-            }
-        }
-        
-        if (this.keys.up.isDown && this.bulb.body.touching.down) {
-            this.bulb.setVelocityY(-200);
-        }
+    if (this.keys.up.isDown && this.bulb.body.touching.down) {
+      this.bulb.setVelocityY(-200);
+    }
+  }
+
+  updateBattery() {
+    const speed = 160;
+
+    if (this.keys.p2left.isDown) {
+      this.battery.setVelocityX(-speed);
+      if (!this.battery.body.touching.down) {
+        this.battery.play("batteryJump", true);
+      } else {
+        this.battery.play("batteryWalk", true);
+      }
+    } else if (this.keys.p2right.isDown) {
+      this.battery.setVelocityX(speed);
+      if (!this.battery.body.touching.down) {
+        this.battery.play("batteryJump", true);
+      } else {
+        this.battery.play("batteryWalk", true);
+      }
+    } else {
+      this.battery.setVelocityX(0);
+      if (!this.battery.body.touching.down) {
+        this.battery.play("batteryJump", true);
+      } else {
+        this.battery.play("batteryWalk", true);
+      }
     }
 
-    updateBattery() {
-        const speed = 160;
+    if (this.keys.p2up.isDown && this.battery.body.touching.down) {
+      this.battery.setVelocityY(-450);
+    }
+  }
 
-        if (this.keys.p2left.isDown) {
-            this.battery.setVelocityX(-speed);
-            if (!this.battery.body.touching.down){
-                this.battery.play('batteryJump', true);
-            }  else {
-                this.battery.play('batteryWalk', true);
-            }
+  updateCamera() {
+    let midpointX = (this.bulb.x + this.battery.x) / 2;
+    this.cameras.main.scrollX = midpointX - this.cameras.main.width / 2;
 
-        } else if (this.keys.p2right.isDown) {
-            this.battery.setVelocityX(speed);
-            if (!this.battery.body.touching.down){
-                this.battery.play('batteryJump', true);
-            }  else {
-                this.battery.play('batteryWalk', true);
-            }
-        } else {
-            this.battery.setVelocityX(0);
-            if (!this.battery.body.touching.down){
-                this.battery.play('batteryJump', true);
-            }  else {
-                this.battery.play('batteryWalk', true);
-            }
-        }
-        
-        if (this.keys.p2up.isDown && this.battery.body.touching.down) {
-            this.battery.setVelocityY(-450);
-        }    
+    let worldBounds = this.physics.world.bounds;
+    this.cameras.main.scrollX = Phaser.Math.Clamp(
+      this.cameras.main.scrollX,
+      0,
+      worldBounds.width - this.cameras.main.width
+    );
+  }
 
+  updateFanBulbCollision() {
+    if (!this.fan.isOn) {
+      this.fan.play("fanOff", true);
+      return;
     }
 
-    updateCamera() {
-        let midpointX = (this.bulb.x + this.battery.x) / 2;
-        this.cameras.main.scrollX = midpointX - this.cameras.main.width / 2;
-        
-        let worldBounds = this.physics.world.bounds;
-        this.cameras.main.scrollX = Phaser.Math.Clamp(this.cameras.main.scrollX, 0, worldBounds.width - this.cameras.main.width);
+    this.fan.play("fanOn", true);
+
+    if (this.physics.overlap(this.bulb, this.fanStream)) {
+      this.bulb.setVelocityY(-300);
     }
-
-    updateFanBulbCollision() {
-        if (!this.fan.isOn) {
-            this.fan.play('fanOff', true);
-            return;
-        }
-
-        this.fan.play('fanOn', true);
-
-        if (this.physics.overlap(this.bulb, this.fanStream)) {
-            this.bulb.setVelocityY(-300);
-        }
-    }
-
-
+  }
 }
 
 export default GameScene;
